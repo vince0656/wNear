42bcef29fadec666acdc0ec94f51a6d2fd7064cb
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-20 17:05:59 +0000

    Change receipient to receiver_id

--- src/lib.rs
+++ src/lib.rs
@@ -94,20 +94,20 @@ impl FungibleToken {
     ///   * The storage difference at the fixed storage price defined in the contract.
     #[payable]
     pub fn deposit(&mut self, amount: U128) {
-        // Proxy through to deposit_to() making the recipient the predecessor
+        // Proxy through to deposit_to() making the receiver_id the predecessor
         self.deposit_to(env::predecessor_account_id(), amount);
     }
 
-    /// Deposit NEAR from the predecessor account and send wNear to a specific recipient
+    /// Deposit NEAR from the predecessor account and send wNear to a specific receiver_id
     /// Requirements:
-    /// * `recipient` cannot be this contract
-    /// * `recipient` must be a valid account Id
+    /// * `receiver_id` cannot be this contract
+    /// * `receiver_id` must be a valid account Id
     /// * `amount` must be a positive integer
     /// * Caller of the method has to attach deposit enough to cover:
     ///   * The `amount` of wNear tokens being minted, and
     ///   * The storage difference at the fixed storage price defined in the contract.
     #[payable]
-    pub fn deposit_to(&mut self, recipient: AccountId, amount: U128) {
+    pub fn deposit_to(&mut self, receiver_id: AccountId, amount: U128) {
         let initial_storage = env::storage_usage();
 
         // As attached deposit includes tokens for storage, deposit amount needs to be explicit
@@ -117,17 +117,17 @@ impl FungibleToken {
         }
 
         assert!(
-            env::is_valid_account_id(recipient.as_bytes()),
+            env::is_valid_account_id(receiver_id.as_bytes()),
             "New owner's account ID is invalid"
         );
 
         assert_ne!(
-            recipient, env::current_account_id(),
+            receiver_id, env::current_account_id(),
             "Invalid transfer to this contract"
         );
 
-        // Mint to recipient
-        self.mint(&recipient, amount.clone());
+        // Mint to receiver_id
+        self.mint(&receiver_id, amount.clone());
 
         // Check we have enough attached deposit
         let current_storage = env::storage_usage();
@@ -173,16 +173,16 @@ impl FungibleToken {
         self.withdraw_to(env::predecessor_account_id(), amount);
     }
 
-    /// Unwraps wNear from the predecessor account and sends the Near to a specific recipient
+    /// Unwraps wNear from the predecessor account and sends the Near to a specific receiver_id
     /// Requirements:
-    /// * `recipient` cannot be this contract
-    /// * `recipient` must be a valid account Id
+    /// * `receiver_id` cannot be this contract
+    /// * `receiver_id` must be a valid account Id
     /// * `amount` should be a positive integer
     /// * Caller must have a balance that is greater than or equal to `amount`.
     /// * Caller of the method has to attach deposit enough to cover storage difference at the
     ///   fixed storage price defined in the contract.
     #[payable]
-    pub fn withdraw_to(&mut self, recipient: AccountId, amount: U128) {
+    pub fn withdraw_to(&mut self, receiver_id: AccountId, amount: U128) {
         let initial_storage = env::storage_usage();
 
         let amount: Balance = amount.into();
@@ -191,30 +191,30 @@ impl FungibleToken {
         }
 
         assert!(
-            env::is_valid_account_id(recipient.as_bytes()),
+            env::is_valid_account_id(receiver_id.as_bytes()),
             "New owner's account ID is invalid"
         );
 
         assert_ne!(
-            recipient, env::current_account_id(),
+            receiver_id, env::current_account_id(),
             "Invalid transfer to this contract"
         );
 
         // Decrease the predecessor's wNear balance and reduce total supply
         self.burn(&env::predecessor_account_id(), amount.clone());
 
-        // Send near `amount` to recipient
+        // Send near `amount` to receiver_id
         env::log(format!("Withdrawal of {} wNear", amount).as_bytes());
-        Promise::new(recipient).transfer(amount);
+        Promise::new(receiver_id).transfer(amount);
 
         self.refund_storage(initial_storage);
     }
 
-    /// The withdraw_from function allows to unwrap wNear from an owner wallet to a recipient wallet
+    /// The withdraw_from function allows to unwrap wNear from an owner wallet to a receiver_id wallet
     /// Requirements:
-    /// * `recipient` of the Near tokens cannot be this contract
-    /// * `recipient` must be a valid account Id
-    /// * `recipient` cannot be the same as `owner_id`. Use `withdraw()` in that scenario.
+    /// * `receiver_id` of the Near tokens cannot be this contract
+    /// * `receiver_id` must be a valid account Id
+    /// * `receiver_id` cannot be the same as `owner_id`. Use `withdraw()` in that scenario.
     /// * `amount` should be a positive integer.
     /// * `owner_id` should have balance on the account greater or equal than the withdraw `amount`.
     /// * If this function is called by an escrow account (`owner_id != predecessor_account_id`),
@@ -224,7 +224,7 @@ impl FungibleToken {
     /// * Caller of the method has to attach deposit enough to cover storage difference at the
     ///   fixed storage price defined in the contract.
     #[payable]
-    pub fn withdraw_from(&mut self, owner_id: AccountId, recipient: AccountId, amount: U128) {
+    pub fn withdraw_from(&mut self, owner_id: AccountId, receiver_id: AccountId, amount: U128) {
         let initial_storage = env::storage_usage();
 
         let amount: Balance = amount.into();
@@ -233,17 +233,17 @@ impl FungibleToken {
         }
 
         assert!(
-            env::is_valid_account_id(recipient.as_bytes()),
+            env::is_valid_account_id(receiver_id.as_bytes()),
             "New owner's account ID is invalid"
         );
 
         assert_ne!(
-            recipient, env::current_account_id(),
+            receiver_id, env::current_account_id(),
             "Invalid transfer to this contract"
         );
 
         assert_ne!(
-            owner_id, recipient,
+            owner_id, receiver_id,
             "The new owner should be different from the current owner"
         );
 
@@ -263,9 +263,9 @@ impl FungibleToken {
 
         self.burn(&owner_id, amount.clone());
 
-        // Send near `amount` to recipient
+        // Send near `amount` to receiver_id
         env::log(format!("Withdrawal of {} wNear", amount).as_bytes());
-        Promise::new(recipient).transfer(amount);
+        Promise::new(receiver_id).transfer(amount);
 
         self.refund_storage(initial_storage);
     }
@@ -424,8 +424,8 @@ impl FungibleToken {
 }
 
 impl FungibleToken {
-    /// Internal method for minting an `amount` to `recipient` AccountId
-    fn mint(&mut self, recipient: &AccountId, amount: Balance) {
+    /// Internal method for minting an `amount` to `receiver_id` AccountId
+    fn mint(&mut self, receiver_id: &AccountId, amount: Balance) {
         if self.total_supply == std::u128::MAX {
             env::panic(b"Total supply limit reached");
         }
@@ -434,9 +434,9 @@ impl FungibleToken {
             env::panic(b"Amount will exceed max permitted total supply");
         }
 
-        let mut account = self.get_account(&recipient);
+        let mut account = self.get_account(&receiver_id);
         account.balance += amount;
-        self.set_account(&recipient, &account);
+        self.set_account(&receiver_id, &account);
 
         // Increase total supply
         self.total_supply += amount;
